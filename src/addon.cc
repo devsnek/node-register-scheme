dd51da8e59ebd138084528e5d981b696551c8c3a
Nicola Del Gobbo <nicoladelgobbo@gmail.com>
2018-05-06 11:48:22 +0200

    The RPH native funxiont now return a boolean value

--- src/addon.cc
+++ src/addon.cc
@@ -5,7 +5,7 @@ Napi::Value RPH(const Napi::CallbackInfo& info) {
   const char* scheme = info[0].As<Napi::String>().Utf8Value().c_str();
   const char* command = info[0].As<Napi::String>().Utf8Value().c_str();
 
-  Register(scheme, command);
+  return Napi::Boolean::New(info.Env(), Register(scheme, command));
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
